883e608d4f83c20b2d8362e44f3fdbab90ade476
David Thong Nguyen <dev@davidtn.com>
2020-11-22 17:56:06 -0500

    Need a way to detect change and keep track of both cursor taret and speaker at the same time

---
+++ src/components/caption.ts
@@ -0,0 +1,23 @@
+import {Entity, registerComponent} from 'aframe';
+import {Vector3} from 'three';
+
+export const captionComponent = registerComponent('caption', {
+  schema: {speaker: {type: 'string'}, cursorTarget: {type: 'string'}},
+  init: function () {
+    console.log(this.data.speaker);
+    console.log(this.data.cursorTarget);
+  },
+  update: function () {
+    console.log(this.data.speaker);
+    console.log(this.data.cursorTarget);
+    if (
+      this.data.speaker &&
+      this.data.cursorTarget &&
+      this.data.speaker === this.data.cursorTarget
+    ) {
+      this.el.setAttribute('opacity', 1);
+    } else {
+      this.el.setAttribute('opacity', 0);
+    }
+  },
+});
