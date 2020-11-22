22d76e1f433d628927ffae1a8fbe2f33f73d4f88
David Thong Nguyen <dev@davidtn.com>
2020-11-22 17:56:19 -0500

    New comp

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ import {cursorListenerComponent} from './components/cursorListener';
 import {beginSocketComponent} from './components/beginSocket';
 import {videoPlaybackSystem} from './systems/videoPlayback';
 import {videoPlaybackComponent} from './components/videoPlayback';
+import {captionComponent} from './components/caption';
 
 // Necessary so webpack won't mark these as dead code. Probably a better way to do this but ¯\_(ツ)_/¯
 messageQueuesComponent;
 messageQueuesSystem;
 rotationReaderComponent;
+captionComponent;
 cursorListenerComponent;
 beginSocketComponent;
 videoPlaybackSystem;
