62fb68c0aa3b8c73b243f8a0d8e651ab76506d8e
David Thong Nguyen <dev@davidtn.com>
2020-11-23 21:37:34 -0500

    Logging rotation instead of position

--- src/components/rotationReader.ts
+++ src/components/rotationReader.ts
@@ -7,17 +7,11 @@ export const rotationReaderComponent = registerComponent('rotation-reader', {
    * vector or euler and not re-create on every tick to save memory.
    */
   tick: (function () {
-    const position = new Vector3();
     return function () {
       // using "this" does not support getting object3D, so we use querySelector instead
-      const camPos = document
-        .querySelector('a-camera')
-        .object3D.getWorldPosition(position);
-      const texts = document.querySelectorAll('a-entity[text]');
-      texts.forEach(t => {
-        const textEntity = t as Entity;
-        textEntity.object3D.lookAt(camPos.x, 1, camPos.z);
-      });
+      const camRotation = document.querySelector('a-camera').object3D.rotation;
+      const {x, y, z} = camRotation;
+      // console.log({x}, {y}, {z});
     };
   })(),
 });
